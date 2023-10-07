cf410e18f621b06ba9b6a2397b4eabbeba134a5a
noctispine <pinefromw@gmail.com>
2023-10-07 23:32:56 +0300

    feat(repo): add get_commits method

--- src/repo.rs
+++ src/repo.rs
@@ -3,7 +3,10 @@ use crate::customerror::{
     Result
 };
 use std::path::Path;
-use git2::Repository;
+use git2::{
+    Repository,
+    Commit, Sort
+};
 use std::io;
 
 
@@ -24,7 +27,28 @@ impl Repo {
             )))
         }
     }
+
+    pub fn get_commits(&self) -> Result<Vec<Commit>> {
+        let mut walk = self.dot_git.revwalk()?;
+		walk.set_sorting(Sort::TOPOLOGICAL)?;
+
+        walk.push_head()?;
+
+        let commits: Vec<Commit> = walk
+            .filter_map(|oid| oid.ok())
+            .filter_map(|oid| self.dot_git.find_commit(oid).ok())
+            .collect();
+
+        Ok(commits)
+    }
+
+    pub fn find_last_commit(&self) -> Result<Commit> {
+        let obj = self.dot_git.head()?.resolve()?.peel(git2::ObjectType::Commit)?;
+        obj.into_commit().map_err(|_| Error::GitError(git2::Error::from_str("Couldn't find the commit")))
+        
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -32,6 +56,8 @@ mod tests {
 
     use super::*;
     use tempfile::TempDir;
+    use std::cell::RefCell;
+
 
     #[test]
     fn test_init_existing_path() {
@@ -56,5 +82,50 @@ mod tests {
 
         assert!(result.is_err());
         temp_dir.close().expect("Failed to close temp dir")
+    }
+
+    #[test]
+    fn can_get_commits() {
+
+        let temp_dir = TempDir::new().expect("Failed to create temp dir");
+        Repository::init(temp_dir.path()).expect("Failed to create temp repository");
+        let repo = RefCell::new(Repo::init(temp_dir.path()).expect("Failed to get the repo"));
+        let b_repo = repo.borrow_mut();
+
+
+        let mut index = b_repo.dot_git.index().unwrap();
+        let oid = index.write_tree().unwrap();
+        let signature = git2::Signature::now("Eren", "erencam.dev@gmail.com").unwrap();
+        let message = "feat(repo): add get_commits";
+        let tree = b_repo.dot_git.find_tree(oid).unwrap();
+
+
+        b_repo.dot_git.commit(
+            Some("HEAD"), 
+            &signature, 
+            &signature, 
+            message, 
+            &tree, 
+            &[]
+        ).unwrap();
+
+        let parent = b_repo.find_last_commit().unwrap();
+
+        b_repo.dot_git.commit(
+            Some("HEAD"), 
+            &signature, 
+            &signature, 
+            message, 
+            &tree, 
+            &[&parent]
+        ).unwrap();
+        
+
+
+        let commits = b_repo.get_commits();
+        assert!(commits.is_ok());
+        assert_eq!(commits.unwrap().len(), 2);
+        
+
     }
 }
