978f22da7a1de3c79c5af3965b345e5b5fa38a15
noctispine <pinefromw@gmail.com>
2023-10-07 21:04:11 +0300

    feat: add argument parser & basic flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+use std::env::current_dir;
+
+// Analyze git repo by conventional commits
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Exclude scope
+    #[arg(short, long)]
+    scope: bool,
+
+    /// Exclude commit type
+    #[arg(short, long)]
+    commit_type: bool,
+
+    /// Repository path
+    #[arg(short, long)]
+    path: Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let path = args.path
+        .unwrap_or((current_dir()
+            .expect("there should be a path"))
+                .to_string_lossy().to_string()
+        );
+    
+    
+    println!("path {}", path);
 }
