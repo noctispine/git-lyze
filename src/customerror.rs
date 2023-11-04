f94ab0d89bc51a1bc2b18143ca572dae5aee76e7
noctispine <pinefromw@gmail.com>
2023-11-04 19:42:02 +0300

    feat(customerror): add parse error

--- src/customerror.rs
+++ src/customerror.rs
@@ -2,12 +2,15 @@ use thiserror::Error as ThisError;
 
 #[derive(Debug, ThisError)]
 pub enum Error {
-    #[cfg(feature= "repo")]
+    #[cfg(feature = "repo")]
     #[error("Git error: `${0}`")]
     GitError(#[from] git2::Error),
 
     #[error("IO error: `{0}`")]
-    IoError(#[from] std::io::Error)
+    IoError(#[from] std::io::Error),
+
+    #[error("Parse error: `{0}`")]
+    ParseError(String),
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
