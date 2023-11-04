a1f1293eae85a96d827e3a3ae8e7dad0d009ea18
noctispine <pinefromw@gmail.com>
2023-11-04 19:41:36 +0300

    style(convention): format

--- src/convention.rs
+++ src/convention.rs
@@ -1,13 +1,11 @@
 use regex::Regex;
 
-
 #[derive(Debug)]
 pub struct ConventionBuilder {
     regex: Regex,
 }
 
-#[derive(Debug)]
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct ParsedCommitInfo {
     pub type_: String,
     pub optional_scope: Option<String>,
@@ -17,31 +15,33 @@ pub struct ParsedCommitInfo {
 const OPTIONAL_SCOPE_INDICATOR: &str = "optional_scope";
 
 impl ConventionBuilder {
-    pub fn build(example_commit_message: String) -> ConventionBuilder {
-        // optional_scope_indicators = ()
-        let indx = example_commit_message.find(OPTIONAL_SCOPE_INDICATOR).expect("There must be an optional scope indicator");
+    pub fn build(example_commit_message: &str) -> ConventionBuilder {
+        let indx = example_commit_message
+            .find(OPTIONAL_SCOPE_INDICATOR)
+            .expect("There must be an optional scope indicator");
         let indicators = (
             example_commit_message.as_bytes()[indx - 1] as char,
-            example_commit_message.as_bytes()[OPTIONAL_SCOPE_INDICATOR.len() + indx] as char
+            example_commit_message.as_bytes()[OPTIONAL_SCOPE_INDICATOR.len() + indx] as char,
         );
 
         let regex_pattern = format!(r"^(.*?)(?:\{}(.*?)\{})?: (.*)$", indicators.0, indicators.1);
 
-        ConventionBuilder { regex: Regex::new(&regex_pattern).unwrap() }
+        ConventionBuilder {
+            regex: Regex::new(&regex_pattern).unwrap(),
+        }
     }
 
     pub fn construct_info(&self, message: String) -> Option<ParsedCommitInfo> {
-        // let captures = self.regex.captures(&message);
         let captures = self.regex.captures(&message);
 
         match captures {
             Some(captures) => Some(ParsedCommitInfo {
                 type_: captures.get(1).unwrap().as_str().to_string(),
                 optional_scope: captures.get(2).map(|m| m.as_str().to_string()),
-                description: captures.get(3).map(|m| m.as_str().to_string())
+                description: captures.get(3).map(|m| m.as_str().to_string()),
             }),
 
-            None => None
+            None => None,
         }
     }
 }
@@ -53,8 +53,10 @@ mod tests {
     #[test]
     fn can_construct_fully_described_commit_message() {
         let example_commit_message = String::from("type(optional_scope): description");
-        let style_builder = ConventionBuilder::build(example_commit_message);
-        let parsed_info = style_builder.construct_info(String::from("ci(frontend): build times")).unwrap();
+        let style_builder = ConventionBuilder::build(example_commit_message.as_str());
+        let parsed_info = style_builder
+            .construct_info(String::from("ci(frontend): build times"))
+            .unwrap();
         assert_eq!(parsed_info.type_, "ci");
         assert_eq!(parsed_info.optional_scope.unwrap(), "frontend");
         assert_eq!(parsed_info.description.unwrap(), "build times");
@@ -63,8 +65,10 @@ mod tests {
     #[test]
     fn can_construct_commit_message_without_optional_scope() {
         let example_commit_message = String::from("type(optional_scope): description");
-        let style_builder = ConventionBuilder::build(example_commit_message);
-        let parsed_info = style_builder.construct_info(String::from("ci: build times")).unwrap();
+        let style_builder = ConventionBuilder::build(example_commit_message.as_str());
+        let parsed_info = style_builder
+            .construct_info(String::from("ci: build times"))
+            .unwrap();
         assert_eq!(parsed_info.type_, "ci");
         assert_eq!(parsed_info.optional_scope, None);
         assert_eq!(parsed_info.description.unwrap(), "build times");
@@ -73,9 +77,8 @@ mod tests {
     #[test]
     fn should_skip_non_conventional_commit() {
         let example_commit_message = String::from("type(optional_scope): description");
-        let style_builder = ConventionBuilder::build(example_commit_message);
+        let style_builder = ConventionBuilder::build(example_commit_message.as_str());
         let parsed_info = style_builder.construct_info(String::from("init"));
         assert!(parsed_info.is_none());
     }
 }
-    
