908d7042e655734dc5bca6bd7842b875caeca7aa
noctispine <pinefromw@gmail.com>
2023-11-30 22:50:07 +0300

    fix(utils): parse date-only format correctly

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,16 @@
-use std::{collections::HashMap, cmp};
+use std::{cmp, collections::HashMap};
 
-use crate::{config::{DateFormatType, Config, SortType}, commit::FileStatInfo};
+use crate::{
+    commit::FileStatInfo,
+    config::{Config, DateFormatType, SortType},
+};
 use chrono::{DateTime, NaiveDate, NaiveDateTime};
 
 pub fn parse_date(value: &String, format: &String, format_type: &DateFormatType) -> Option<i64> {
     match format_type {
         DateFormatType::DateOnly => NaiveDate::parse_from_str(&value, &format)
             .ok()
-            .map(|date| date.and_hms_opt(12, 0, 0).unwrap_or_default())
+            .map(|date| date.and_hms_opt(0, 0, 0).unwrap_or_default())
             .map(|date| date.timestamp()),
 
         DateFormatType::DateAndTime => NaiveDateTime::parse_from_str(&value, &format)
@@ -39,4 +42,3 @@ pub fn map_file_summs<'a>(
 
     summ[..boundry].to_vec()
 }
-
