d7f687b98c8ab50f7515a3277e4add7782447f9c
noctispine <pinefromw@gmail.com>
2024-01-08 00:04:02 +0300

    chore(lib): add logs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
+pub mod cache;
 pub mod commit;
 pub mod config;
 pub mod convention;
 pub mod customerror;
+pub mod defaults;
 pub mod ownerships;
 pub mod repo;
 pub mod reporters;
 pub mod test_utils;
+pub mod tracker;
 pub mod utils;
-pub mod defaults;
 
+use crate::utils::set_log_env;
 use std::{env::current_dir, fs::File, path::Path};
 
 use clap::Parser;
+use colored::Color;
 use reporters::{BaseReporter, Stdout};
+use tracker::Tracker;
 
 use crate::{commit::CommitBucket, config::Config, repo::Repo};
 
@@ -28,17 +33,27 @@ pub fn run() {
         config
     };
 
+    set_log_env(&config.log_level);
+    env_logger::init();
+    let mut perf_tracker = Tracker::new(&config, Color::Magenta, None);
+
     let path = config.path.clone().unwrap_or(
         (current_dir().expect("there should be a path"))
             .to_string_lossy()
             .to_string(),
     );
 
+    perf_tracker.start("init repo");
     let repo = Repo::init(Path::new(&path)).unwrap();
+    perf_tracker.stop();
 
+    perf_tracker.start("collect commit bucket");
     let commit_bucket = CommitBucket::build(&repo, &config.convention_style, &config).unwrap();
+    perf_tracker.stop();
 
+    perf_tracker.start("create reporter");
     let base_reporter = BaseReporter::new(&config, &commit_bucket, Box::new(Stdout {}));
+    perf_tracker.stop();
 
     base_reporter.output();
 }
